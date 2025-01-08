35093738d534ea6f070090e7c57399b3ab4365fd
ranyoung <fks1311@gmail.com>
2025-01-08 18:19:21 +0900

    globalStyles 수정

--- src/lib/GlobalStyles.js
+++ src/lib/GlobalStyles.js
@@ -6,9 +6,9 @@ import reset from "styled-reset";
 const GlobalStyles = createGlobalStyle`
   html {
     ${reset};
-    font-size:62.5%;
-    padding: 2em 5em;
-    background-color: black;
+    font-size: 62.5%;
+    padding: 2em 3em;
+    background-color: #0b0c0f;
   }
 `;
 
