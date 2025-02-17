54df5205cae8ac489bcf4f06607c988f1ede1014
ranyoung <fks1311@gmail.com>
2025-02-17 16:24:07 +0900

    README 및 프로젝트 디테일 정보 수정

--- src/lib/project.js
+++ src/lib/project.js
@@ -30,8 +30,8 @@ export default function projectJson() {
       thumbnails: blank,
       subject: "MovieZip",
       project_detail:
-        "현재 보고 계신 포트폴리오이며, Next.js 프레임워크를 사용했습니다. 현재 이 포트폴리오는 app router 방식으로 설계되어 있으며, styled-components와 framer-motion의 라이브러리를 활용하였습니다.",
-      use_tech: "Next.js, styled-components, framer-motion",
+        "Next.js의 page router 기반으로 설계된 영화 정보 제공 사이트입니다. 일별/주간 박스오피스 정보와 장르/연도/국가별 영화 정보 목록을 확인 할 수 있으며, 관련 데이터들은 한국영상자료원과 영화진흥위원회 Open API를 통해 제공받았습니다. 해당 프로젝트에서는 Next.js의 page router의 데이터 Fetching과 react-query의 데이터 Fetching 두 가지 방법으로 데이터 관리가 이루어졌습니다.",
+      use_tech: "Next.js, react-query, axios, styled-components, framer-motion",
       project_images: [blank, blank, blank],
     },
   ];
