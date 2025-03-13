461fe90d6e9fbd42fa4b3b5064f0eadf0f2341a7
ranyoung <fks1311@gmail.com>
2025-03-14 00:01:48 +0900

    project page 이동 및 css 적용

--- src/app/project/[page]/page.js
+++ src/app/project/[page]/page.js
@@ -8,22 +8,55 @@ import { useParams, useRouter } from "next/navigation";
 import projectJson from "@/lib/project";
 import decodedHTML from "@/lib/decodedHTML";
 import Metadata from "@/components/Metadata";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
+import { MdClose } from "react-icons/md";
 
 export default function DetailPage() {
   const params = useParams();
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true); // 다른 방법
   const router = useRouter();
+  const [loading, setLoading] = useState(true); // 다른 방법
+  const [projects, setProjects] = useState([]); // 현 프로젝트 정보
+  const [list, setList] = useState([]);
+  const [curIdx, setCurIdx] = useState();
 
   useEffect(() => {
     const filter = projectJson().filter((f) => f.subject === decodedHTML(params.page));
     setProjects(filter);
     setLoading(false);
+    const project = projectJson().map((item) => ({ subject: item.subject }));
+    const currentIdx = project.findIndex((item) => item.subject === filter[0].subject);
+    setList(project);
+    setCurIdx(currentIdx);
   }, [params.page]);
 
-  const onClick = (subject, link) => {
-    console.log(subject);
+  const liveClick = (subject, link) => {
     subject === "Portfolio" ? router.push(link) : window.open(link, "_blank");
+  };
+
+  const prevClick = (i) => {
+    if (i === 0) {
+      router.push(`/project/${list[list.length - 1].subject}`);
+    } else {
+      router.push(`/project/${list[i - 1].subject}`);
+    }
+  };
+  const nextClick = (i) => {
+    if (i === list.length - 1) {
+      router.push(`/project/${list[0].subject}`);
+    } else {
+      router.push(`/project/${list[i + 1].subject}`);
+    }
+  };
+
+  const underlineVariants = {
+    initial: { width: "100%" },
+    visible: { width: "100%" },
+    hover: {
+      rotateX: 360,
+      transition: {
+        duration: 0.8,
+      },
+    },
   };
 
   return (
@@ -36,7 +69,7 @@ export default function DetailPage() {
             <Fragment key={i}>
               <Metadata title={project.subject} />
               <Project>
-                <div>
+                <div className="project-div">
                   <p className="subject">project</p>
                   <p>{project.subject}</p>
                 </div>
@@ -48,7 +81,29 @@ export default function DetailPage() {
                   <p className="subject">use tech</p>
                   <p>{project.use_tech}</p>
                 </div>
-                <ViewLive onClick={() => onClick(project.subject, project.link)}>VIEW LIVE PROJECT</ViewLive>
+                <ViewLive
+                  variants={underlineVariants}
+                  initial="initial"
+                  animate="visible"
+                  whileHover="hover"
+                  onClick={() => liveClick(project.subject, project.link)}
+                >
+                  VIEW LIVE PROJECT
+                </ViewLive>
+                <ListBtn>
+                  <div className="btn" onClick={() => prevClick(curIdx)}>
+                    <FaArrowLeft />
+                    previous project
+                  </div>
+                  <div className="btn" onClick={() => router.push("/project")}>
+                    <MdClose />
+                    close
+                  </div>
+                  <div className="btn" onClick={() => nextClick(curIdx)}>
+                    <FaArrowRight />
+                    next project
+                  </div>
+                </ListBtn>
               </Project>
               <Frame>
                 {project.project_images.map((img, idx) => (
@@ -81,7 +136,7 @@ const Project = styled.div`
   flex-direction: column;
   gap: 5rem;
   line-height: 2.5rem;
-  div {
+  .project-div {
     display: flex;
     flex-direction: column;
     gap: 1rem;
@@ -100,15 +155,27 @@ const Frame = styled(motion.div)`
   border-radius: 10px;
   img {
     height: 70vh;
+    width: 55vw;
     border-radius: 10px;
     border: 1px solid white;
     margin: 1rem;
   }
 `;
-const ViewLive = styled.div`
-  text-align: center;
+const ViewLive = styled(motion.div)`
+  display: flex;
+  justify-content: center;
   line-height: 5rem;
   letter-spacing: 0.3rem;
   border-bottom: 1px solid white;
   cursor: pointer;
+`;
+const ListBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+  .btn {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
 `;
