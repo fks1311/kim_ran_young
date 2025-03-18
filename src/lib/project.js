b873081d059b1d4104e766ab53d2b71c620f661f
ranyoung <fks1311@gmail.com>
2025-03-19 00:45:34 +0900

    mvz main 추가

--- src/lib/project.js
+++ src/lib/project.js
@@ -1,15 +1,12 @@
 import blank from "@/lib/blankImg.svg";
+// portfolio
 import portfolio_main from "@/assets/portfolio/portfolio-main.png";
 import home_gif from "@/assets/portfolio/home.gif";
 import about_gif from "@/assets/portfolio/about.gif";
-/**
- * subject
- * tech
- * introduce letter
- * hard
- * new tech
- * img
- */
+
+// moviezip
+import mvz_main from "@/assets/moviezip/home-main.png";
+
 export default function projectJson() {
   return [
     {
@@ -31,12 +28,12 @@ export default function projectJson() {
       link: "https://github.com/fks1311",
     },
     {
-      thumbnails: blank,
+      thumbnails: mvz_main,
       subject: "MovieZip",
       project_detail:
         "Next.js의 page router 기반으로 설계된 영화 정보 제공 사이트입니다. 일별/주간 박스오피스 정보와 장르/연도/국가별 영화 정보 목록을 확인 할 수 있으며, 관련 데이터들은 한국영상자료원과 영화진흥위원회 Open API를 통해 제공받았습니다. 해당 프로젝트에서는 Next.js의 page router의 데이터 Fetching과 react-query의 데이터 Fetching 두 가지 방법으로 데이터 관리가 이루어졌습니다.",
       use_tech: "Next.js, react-query, axios, styled-components, framer-motion",
-      project_images: [blank, blank, blank],
+      project_images: [mvz_main, blank, blank],
       link: "https://velog.io/@well_log/posts",
     },
   ];
